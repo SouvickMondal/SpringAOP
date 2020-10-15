ef6041899b5da7915971e82375338701efb4222c
unknown <souvick@ALCHEMYCLOUD.CO.IN>
2020-10-15 14:28:37 +0800

    Souvick AOP and Log File program

---
+++ Day15AOP/src/com/training/org/BeforeAdvice.java
@@ -0,0 +1,27 @@
+package com.training.org;
+
+import java.lang.reflect.Method;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+
+import org.springframework.aop.MethodBeforeAdvice;
+
+public class BeforeAdvice implements MethodBeforeAdvice{
+
+	@Override
+	public void before(Method arg0, Object[] arg1, Object arg2) throws Throwable {
+		System.out.println("Additional concern before logic");
+		System.out.println("Method infor"+arg0.getName());
+		for(Object arg:arg1) {
+			System.out.println(arg);
+		}
+		System.out.println("Target "+arg2);
+		System.out.println("Target class"+arg2.getClass().getName());
+		FileHandler handler = new FileHandler("default.log", true);
+		Logger l = Logger.getLogger("com.javacodegeeks.snippets.core");
+	       	l.addHandler(handler);
+	        l.info("Before Souvick");
+		
+	}
+}
+
